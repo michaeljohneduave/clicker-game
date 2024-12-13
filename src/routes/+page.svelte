912c1e43763084ea80eb708421e2a1c7706503cf
Michael <michaeljohneduave@gmail.com>
2024-12-13 15:33:04 -0800

    Initial rendering of downtown vancouver with overpass elements rendered on map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import osmtogeojson from 'osmtogeojson';
+	import L from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
+
+	async function fetchElements(lat, lon) {
+		const response = await fetch('https://overpass-api.de/api/interpreter', {
+			method: 'POST',
+			body: `
+      [out:json][timeout:10];
+      (
+        way["building"](49.2754,-123.1322,49.2904,-123.1147); // Downtown Vancouver bounding box
+        relation["building"](49.2754,-123.1322,49.2904,-123.1147);
+      );
+      out geom;
+      `
+		});
+
+		const data = await response.json();
+		console.log(data.elements);
+
+		return osmtogeojson(data);
+	}
+
+	onMount(async () => {
+		const map = L.map('map').setView([49.24966, -123.11934], 13);
+		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 19,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(map);
+
+		const elements = await fetchElements(49.24966, -123.11934);
+		L.geoJSON(elements).addTo(map);
+	});
+</script>
+
+<div id="map">LOL</div>
+
+<style>
+	#map {
+		height: 100vh;
+	}
+</style>
